269c05714156944094a2c64aa3eea18215f067e4
ApichaiTi <81065469+artijo@users.noreply.github.com>
2024-11-24 00:26:51 +0700

    Refactor App component layout and styles; enhance login page with form fields and validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,168 @@
-import { useState } from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import menuIcon from "./assets/icons/menu-svgrepo-com.svg";
 
 function App() {
-
   const navLinks = [
-    { name: 'แดชบอร์ด', path: '/' },
-    { name: 'นักเรียน', path: '/students' },
-    { name: 'ครู', path: '/teachers' },
-    { name: 'ห้องเรียน', path: '/classroom' },
-    { name: 'วิชาเรียน', path: '/subjects' },
-    { name: 'กิจกรรม', path: '/activities' },
-    { name: 'การเข้าเรียน', path: '/attendances' },
-    { name: 'คำร้อง', path: '/leavereq' },
-    { name: 'ตั้งค่า', path: '/settings' },
-  ]
+    { name: "แดชบอร์ด", path: "/" },
+    { name: "นักเรียน", path: "/students" },
+    { name: "ครู", path: "/teachers" },
+    { name: "ห้องเรียน", path: "/classroom" },
+    { name: "วิชาเรียน", path: "/subjects" },
+    { name: "กิจกรรม", path: "/activities" },
+    { name: "การเข้าเรียน", path: "/attendances" },
+    { name: "คำร้อง", path: "/leavereq" },
+    { name: "ตั้งค่า", path: "/settings" },
+  ];
 
   function openMenu() {
-    document.querySelector('header').classList.toggle('active')
+    document.querySelector("header").classList.toggle("active");
   }
 
   return (
     <>
-      <header className='bg-slate-300 sticky left-0 top-0 md:bottom-0 md:fixed md:w-56 h-14 md:h-full'>
-        <div className='p-2 md:p-3 text-white bg-slate-400 flex sm:block justify-between h-14'>
-          <div id='toggle' className='sm:hidden' onClick={openMenu}>menu</div>
-          <h1 className='text-center md:text-left'>ระบบจัดการโรงเรียน</h1>
-          <div className='sm:hidden'>ออกจากระบบ</div>
+      <header className="bg-slate-300 sticky left-0 top-0 md:bottom-0 md:fixed md:w-56 h-14 md:h-full">
+        <div className="p-2 md:p-3 text-white bg-slate-400 flex sm:block justify-between items-center h-14">
+          <div
+            id="toggle"
+            className="sm:hidden flex items-center gap-1 border-2 my-2 p-2"
+            onClick={openMenu}
+          >
+            <svg
+              width={25}
+              viewBox="-2.4 -2.4 28.80 28.80"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                stroke="#CCCCCC"
+                strokeWidth="0.624"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  d="M4 6H20M4 12H20M4 18H20"
+                  stroke="#ffffff"
+                  strokeWidth="1.2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>{" "}
+              </g>
+            </svg>
+            <span className="text-sm">MENU</span>
+          </div>
+          <h1 className="text-center text-xl md:text-left">
+            ระบบจัดการโรงเรียน
+          </h1>
+          <div className="sm:hidden">
+            <svg
+              width={25}
+              fill="#ffffff"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke="#ffffff"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path d="M4,12a1,1,0,0,0,1,1h7.59l-2.3,2.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l4-4a1,1,0,0,0,.21-.33,1,1,0,0,0,0-.76,1,1,0,0,0-.21-.33l-4-4a1,1,0,1,0-1.42,1.42L12.59,11H5A1,1,0,0,0,4,12ZM17,2H7A3,3,0,0,0,4,5V8A1,1,0,0,0,6,8V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V16a1,1,0,0,0-2,0v3a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V5A3,3,0,0,0,17,2Z"></path>
+              </g>
+            </svg>
+          </div>
         </div>
-          <nav className='md:p-3'>
-            <ul className='grid grid-cols-5 gap-3 md:flex md:flex-col md:gap-4'>
-              {navLinks.map((link, index) => (
-                <li key={index}>
-                  <Link to={link.path} className='md:block text-white hover:bg-slate-400 sub'>{link.name}</Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-          <button className='p-2 md:p-3 hidden md:block'>ออกจากระบบ</button>
+        <nav>
+          <ul className="grid grid-cols-5 md:block">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <Link
+                  to={link.path}
+                  className="h-full flex flex-col md:flex-row items-center content-center gap-2 border-s-[3px] border-transparent px-4 py-3 text-gray-500 hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-5 opacity-75"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+
+                  <span className="text-sm text-center font-medium">
+                    {link.name}
+                  </span>
+                </Link>
+              </li>
+            ))}
+            <li>
+              <a
+                href="#"
+                className="h-full flex items-center gap-2 md:border-s-[3px] border-blue-500 bg-blue-50 px-4 py-3 text-blue-700"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5 opacity-75"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+
+                <span className="text-sm font-medium"> General </span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="hidden mt-10 ml-4 md:flex items-center">
+          <svg
+            width={25}
+            fill="none"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="currentColor"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path d="M4,12a1,1,0,0,0,1,1h7.59l-2.3,2.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l4-4a1,1,0,0,0,.21-.33,1,1,0,0,0,0-.76,1,1,0,0,0-.21-.33l-4-4a1,1,0,1,0-1.42,1.42L12.59,11H5A1,1,0,0,0,4,12ZM17,2H7A3,3,0,0,0,4,5V8A1,1,0,0,0,6,8V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V16a1,1,0,0,0-2,0v3a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V5A3,3,0,0,0,17,2Z"></path>
+            </g>
+          </svg>
+          <button className="p-2 md:p-3">ออกจากระบบ</button>
+        </div>
       </header>
-      <main className='md:ml-56'>
+      <main className="md:ml-56 p-4">
         <Outlet />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,22 @@
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-      <p>ส่วนเนื้อหาล็อกอิน</p>
+    <div className="max-w-80">
+      <h1>เข้าสู่ระบบ</h1>
+<div>
+  <label htmlFor="username" className="block text-sm font-medium text-gray-700"> Email </label>
+  <input
+    type="text"
+    id="username"
+    placeholder="username@mail.com"
+    className="mt-1 w-full h-10 rounded-md border-gray-200 shadow-sm sm:text-sm"
+  />
+  <label htmlFor="password" className="block text-sm font-medium text-gray-700"> Password </label>
+  <input
+    type="password"
+    id="password"
+    className="mt-1 w-full h-10 rounded-md border-gray-200 shadow-sm sm:text-sm"
+  />
+</div>
     </div>
   );
 }
