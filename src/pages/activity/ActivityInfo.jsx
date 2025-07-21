f531f6245c1f9ee1a8be0e0f31dbd0b5e8188b01
Peerapon Loa-suangkoon <125670874+PeeraponWWW@users.noreply.github.com>
2025-07-21 11:46:27 +0700

    แก้ Pagination  ไม่ทำงาน

--- src/pages/activity/ActivityInfo.jsx
+++ src/pages/activity/ActivityInfo.jsx
@@ -187,7 +187,7 @@ function ActivityInfo() {
                             </tr>
                         </thead>
                         <tbody>
-                            {activityHistoryProcessed.map((act) => (
+                            {sliceActivityHistoryProcessedList.map((act) => (
                                 <tr key={act.date} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
 
                                     <th scope="row" className="px-6 py-4 font-bold text-gray-900 whitespace-nowrap dark:text-white">
